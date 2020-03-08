27ddad814ab2872ad845f57416de6b234528a68a
AmoghavarshaP <60764108+AmoghavarshaP@users.noreply.github.com>
2020-03-08 15:00:47 -0500

    Add files via upload
    Initial output from histogram equalization, sharpens the image but is quite subtle. will working on it to improve the output.

---
+++ Codes/Q1_histo.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+# Equalization of Histogram can only be applied on grayscale images as a result the following function
+# def EqualizeHistogram(frame):
+#     new_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+#     clahe= cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+#     new = clahe.apply(new_img)
+#     return cv2.cvtColor(new,cv2.COLOR_GRAY2BGR)
+#     # clahe= cv2.createCLAHE()
+#     # new = clahe.apply(new_img)
+#     # new_img = cv2.equalizeHist(new_img)
+#     # return cv2.cvtColor(new_img,cv2.COLOR_GRAY2BGR)
+
+def EqualizeHistogram(frame):
+    new_img = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    clahe= cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    new_img[:,:,2] = clahe.apply(new_img[:,:,2])
+    return cv2.cvtColor(new_img,cv2.COLOR_HSV2BGR)
+
+file='output_histogram3.avi'
+writer = cv2.VideoWriter(file, cv2.VideoWriter_fourcc(*"MJPG"), 30,(1920, 1080))
+
+cap = cv2.VideoCapture('Night Drive - 2689.mp4')
+
+while(True):
+    ret, frame = cap.read()
+    if ret == True:
+        #addwweight method:
+        # gaussian=cv2.GaussianBlur(frame,(7,7),0)
+        # frame1= cv2.addWeighted(frame, 3,gaussian,2, 0)
+        frame1= EqualizeHistogram(frame)
+        frame_1=cv2.resize(frame,None,fx=0.25,fy=0.25,interpolation=cv2.INTER_AREA)
+        frame_new=cv2.resize(frame1,None,fx=0.25,fy=0.25,interpolation=cv2.INTER_AREA)
+        writer.write(frame_new)
+        both = np.concatenate((frame_1,frame_new), axis=1)
+        cv2.imshow('Original and processed:Histogram Equalization', both)
+        # cv2.imshow('original',frame_1)
+        # cv2.imshow('Histogram Equalization', frame_new)
+        if cv2.waitKey(1) & 0xFF == ord('q'):  # press q to quit
+            break
+
+cap.release()
+cv2.destroyAllWindows()
