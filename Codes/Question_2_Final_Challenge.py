2153d7d980c866910bb985e25c06bae79852ee3f
Naman Gupta <namanryan@gmail.com>
2020-03-11 16:56:23 -0400

    Code updated with polylines

--- Codes/Question_2_Final_Challenge.py
+++ Codes/Question_2_Final_Challenge.py
@@ -66,6 +66,35 @@ def undistortImage(warped):
     return dst
 
 
+# function to inverse warp
+def invWarpImage(den_image, pts):
+    # store points
+    tl = pts[0]
+    tr = pts[1]
+    br = pts[2]
+    bl = pts[3]
+    rect = np.array([tl, tr, br, bl], dtype="float32")
+    # rect = (tl,tr,br,bl)
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+    # dst = np.array([[0,0],[199,0],[199,199],[0,199]], dtype="float32")
+
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(den_image, np.linalg.inv(M), (crop.shape[1], crop.shape[0]))
+    return warped
+
+
 if __name__ == '__main__':
     # read image
     cap = cv2.VideoCapture("data_2/challenge_video.mp4")
@@ -92,11 +121,12 @@ if __name__ == '__main__':
 
         # crop real image
         crop = denoise_img[350:720, 0:1280]
-        cv2.imshow('ROI', crop)
+        # cv2.imshow('ROI', crop)
 
         # call mouse click function
         # points = np.float32([[530, 522], [793, 514], [951, 613], [347, 625]])
-        points = np.float32([[463, 115], [860, 119], [1098, 216], [285, 206]])
+        # points = np.float32([[463, 115], [860, 119], [1098, 216], [285, 206]])
+        points = np.float32([[538, 64], [795, 67], [1197, 158], [247, 154]])
         # points = []
 
         # for mouse click to get four points
@@ -112,7 +142,7 @@ if __name__ == '__main__':
 
         # get warped image
         warped_img, M, maxWidth, maxHeight = warpImage(points)
-        cv2.imshow("warped image", warped_img)
+        # cv2.imshow("warped image", warped_img)
         # cv2.waitKey(0)
 
         # extract edges
@@ -142,24 +172,39 @@ if __name__ == '__main__':
 
         # final mask
         mask = cv2.add(yellow_mask, white_mask)
-        cv2.imshow('mask', mask)
+        # cv2.imshow('mask', mask)
 
         # pixel count
         pixel_sum_yellow = np.sum(yellow_mask, axis=0)
         pixel_sum_white = np.sum(white_mask, axis=0)
 
-        # extracting location of pixels
-        pts_yellow = []
-        for i in range(len(pixel_sum_yellow)):
-            if pixel_sum_yellow[i-1] < pixel_sum_yellow[i] < pixel_sum_yellow[i+1]:
-                pts_yellow.append([i, mask.shape[0]])
-                pts_yellow.append([i, 0])
+        # plotting curve
+        # gray = cv2.cvtColor(yellow_mask, cv2.COLOR_RGB2GRAY)
+        # (contours, _) = cv2.findContours(gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        # # biggest area
+        # target = max(contours, key=lambda x: cv2.contourArea(x))
+        # cv2.drawContours(warped_img, [target], -1, [0, 0, 255], -1)  # debug
+        # # just example of fitting
+        # x = target[:, :, 0].flatten()
+        # y = target[:, :, 1].flatten()
+        # poly = np.poly1d(np.polyfit(x, y, 5))
+        # for _x in range(min(x), max(x), 5):  # too lazy for line/curve :)
+        #     cv2.circle(warped_img, (_x, int(poly(_x))), 3, [0, 255, 0])
 
-        pts_white = []
-        for i in range(len(pixel_sum_white)):
-            if pixel_sum_white[i-1] < pixel_sum_white[i] < pixel_sum_white[i+1]:
-                pts_white.append([i, mask.shape[0]])
-                pts_white.append([i, 0])
+        cv2.imshow('warped', warped_img)
+
+        # # extracting location of pixels
+        # pts_yellow = []
+        # for i in range(len(pixel_sum_yellow)):
+        #     if pixel_sum_yellow[i]:
+        #         for j in range(yellow_mask.shape[0]):
+        #             pts_yellow.append([i, j])
+        #
+        # pts_white = []
+        # for i in range(len(pixel_sum_white)):
+        #     if pixel_sum_white[i]:
+        #         for j in range(white_mask.shape[0]):
+        #             pts_white.append([i, j])
 
         # plot histogram
         # plt.plot(pixel_sum)
@@ -168,19 +213,31 @@ if __name__ == '__main__':
         # plt.show()
 
         # line on the image
-        pts_yellow = np.array(pts_yellow)
-        pts_yellow = pts_yellow.reshape((-1, 1, 2))
-        warped_img = cv2.polylines(warped_img, [pts_yellow], True, (0, 0, 255))
-
-        pts_white = np.array(pts_white)
-        pts_white = pts_white.reshape((-1, 1, 2))
-        warped_img = cv2.polylines(warped_img, [pts_white], True, (255, 0, 0))
-
-        cv2.imshow('polylines', warped_img)
-
-        # unwarp the image
-        inv_warped = cv2.warpPerspective(warped_img, np.linalg.inv(M), (maxWidth, maxHeight))
-        cv2.imshow('inv_warped', inv_warped)
+        # pts_yellow = np.array(pts_yellow)
+        # pts_yellow = pts_yellow.reshape((-1, 1, 2))
+        # warped_img = cv2.polylines(warped_img, [pts_yellow], False, (0, 0, 255))
+        # # for i in range(len(pts_yellow)):
+        # #     pt = pts_yellow[i]
+        # #     cv2.circle(warped_img, tuple(pt), 1, [0, 0, 255])
+        #
+        # pts_white = np.array(pts_white)
+        # pts_white = pts_white.reshape((-1, 1, 2))
+        # warped_img = cv2.polylines(warped_img, [pts_white], False, (255, 0, 0))
+        # # for i in range(len(pts_white)):
+        # #     pt = pts_white[i]
+        # #     cv2.circle(warped_img, tuple(pt), 1, [255, 0, 0])
+        #
+        # cv2.imshow('polylines', warped_img)
+        #
+        # # unwarp the image
+        # inv_warped_image = invWarpImage(warped_img, points)
+        # # inv_warped_image = cv2.add(crop, inv_warped_image)
+        # inv_warped_gray = cv2.cvtColor(inv_warped_image, cv2.COLOR_BGR2GRAY)
+        # _, inv_warped_thresh = cv2.threshold(inv_warped_gray, 0, 250, cv2.THRESH_BINARY_INV)
+        # fram_bit = cv2.bitwise_and(crop, crop, mask=inv_warped_thresh)
+        # # lena_warp = cv2.warpPerspective(lena_img, new_homo, (frame.shape[1], frame.shape[0]))
+        # new_frame = cv2.add(fram_bit, inv_warped_image)
+        # cv2.imshow('warped', new_frame)
 
         # ctr += 1
         # print(ctr)
