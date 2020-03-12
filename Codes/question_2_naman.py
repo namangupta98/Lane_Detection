2e3dc15bd50be875c1d7118d9025345119058555
Naman Gupta <namanryan@gmail.com>
2020-03-11 20:12:45 -0400

    modified till unwarping

--- Codes/question_2_naman.py
+++ Codes/question_2_naman.py
@@ -66,9 +66,39 @@ def undistortImage(warped):
     return dst
 
 
+# function to inverse warp
+def invWarpImage(den_image, pts):
+    # store points
+    tl = pts[0]
+    tr = pts[1]
+    br = pts[2]
+    bl = pts[3]
+    rect = np.array([tl, tr, br, bl], dtype="float32")
+    # rect = (tl,tr,br,bl)
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+    # dst = np.array([[0,0],[199,0],[199,199],[0,199]], dtype="float32")
+
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(den_image, np.linalg.inv(M), (crop.shape[1], crop.shape[0]))
+    return warped
+
+
 if __name__ == '__main__':
     # read image
     cap = cv2.VideoCapture("data_1/out.avi")
+    ctr = 0
 
     while True:
 
@@ -95,16 +125,19 @@ if __name__ == '__main__':
         # cv2.imshow('ROI', crop)
 
         # call mouse click function
-        # points = np.float32([[485, 309], [808, 304], [1114, 480], [80, 473]])
-        # points = np.float32([[481, 235], [717, 236], [864, 351], [249, 341]])
-        points = np.float32([[451, 81], [698, 77], [798, 178], [214, 182]])
         # points = []
-        # cv2.namedWindow("image", 1)
-        # cv2.setMouseCallback("image", mouse_click)
+        points = np.float32([[468, 54], [685, 54], [820, 154], [155, 154]])
 
-        # cv2.imshow("image", crop)
-        # if cv2.waitKey(0) & 0xFF == ord('q'):
-        #     cv2.destroyAllWindows()
+        # for mouse click to get four points
+        # if ctr == 220:
+        #
+        #     cv2.namedWindow("frame", 1)
+        #     cv2.setMouseCallback("frame", mouse_click)
+        #     cv2.imshow('frame', crop)
+        #     cv2.waitKey(0)
+        #
+        #     if 0xff == ord('q'):
+        #         break
 
         # get warped image
         warped_img = warpImage(crop, points)
@@ -119,12 +152,12 @@ if __name__ == '__main__':
 
         # color separation using HSV
         hsv = cv2.cvtColor(warped_img, cv2.COLOR_BGR2HSV)
-        lower_white = np.array([0, 0, 183])
+        lower_white = np.array([0, 0, 200])
         higher_white = np.array([255, 255, 255])
         mask = cv2.inRange(hsv, lower_white, higher_white)
         cv2.imshow('mask', mask)
 
-        # pixel count
+        # # pixel count
         pixel_sum = np.sum(mask, axis=0)
 
         # plot histogram
@@ -132,6 +165,32 @@ if __name__ == '__main__':
         # plt.xlabel('Image Cols')
         # plt.ylabel('Sum of Pixels')
         # plt.show()
+
+        pts_white = []
+        for i in range(len(pixel_sum)):
+            if pixel_sum[i]:
+                for j in range(mask.shape[0]):
+                    pts_white.append([i, j])
+
+        pts_white = np.array(pts_white)
+        pts_white = pts_white.reshape((-1, 1, 2))
+        warped_img = cv2.polylines(warped_img, [pts_white], False, (255, 0, 0))
+
+        cv2.imshow('lines', warped_img)
+
+        # unwarp the image
+        inv_warped_image = invWarpImage(warped_img, points)
+        # inv_warped_image = cv2.add(crop, inv_warped_image)
+        inv_warped_gray = cv2.cvtColor(inv_warped_image, cv2.COLOR_BGR2GRAY)
+        _, inv_warped_thresh = cv2.threshold(inv_warped_gray, 0, 250, cv2.THRESH_BINARY_INV)
+        fram_bit = cv2.bitwise_and(crop, crop, mask=inv_warped_thresh)
+        # lena_warp = cv2.warpPerspective(lena_img, new_homo, (frame.shape[1], frame.shape[0]))
+        new_frame = cv2.add(fram_bit, inv_warped_image)
+        cv2.imshow('warped', new_frame)
+
+        # ctr += 1
+        # print(ctr)
+
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
 
